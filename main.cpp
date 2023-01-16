b9490b4391c410c59123c025e0104236580bc1b8
Davide D'Ambrosio <davide431@outlook.it>
2023-01-16 19:49:53 +0100

    Inserita la funziona per la ricerca dell'auto
    Inserita la funziona per la ricerca dell'auto, da definire ancora le modalità di salvataggio della riga

--- main.cpp
+++ main.cpp
@@ -1,9 +1,75 @@
 #include <iostream>
-
+#include <fstream>
+#include <algorithm>
 using namespace std;
+
+struct macchina{
+    string categoria, marca, modello, colore;
+    char lun, mar, mer, gio, ven, sab, dom;
+};
+
+string found_car[100];
+int c = 0;
+
+void cerca_auto(macchina car){
+    ifstream fin("auto.csv", ios::in);
+    if(!fin)
+    cout<<"Errore di apertura del file";
+
+    cout<<"Inserisci la categoria dell'auto: ";
+    cin>>car.categoria;
+
+    c = 0;
+    string cat, app, res;
+    char v = ',';
+
+    while(!fin.eof()){
+        fin>>cat;
+        cat.erase(remove(cat.begin(),cat.end(),v),cat.end());   // Formula che serve per eliminare la virgola
+        if(cat == car.categoria){
+            getline(fin, app);
+            res = cat + "," + app;
+            found_car[c] = res;
+            c++;
+        }
+    }
+    fin.close();
+}
+
+void stampa_file(){
+}
+
+void menu(macchina car)
+{
+    int sc;
+    do {
+    cout << endl;
+		cout << "------------------- MENU' -------------------" << endl;
+		cout << "1 - Cerca auto" << endl
+             << "2 - Stampa file" << endl
+		     << "3 - Uscita" << endl << endl
+		     << "Inserire scelta >> ";
+		cin >> sc;
+		switch (sc)
+		{
+			case 1:
+                cerca_auto(car);
+				break;
+			case 2:
+                stampa_file();
+			    break;
+            case 3:
+                cout<<endl<<"Uscita..."<<endl;
+                break;
+			default:
+				cout << endl << "Scelta sbagliata" << endl << endl;
+		}
+       } while (sc != 3);
+}
 
 int main()
 {
-    cout << "Prova commit github" << endl;
+    macchina car;
+    menu(car);
     return 0;
 }
