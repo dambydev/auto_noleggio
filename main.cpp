f8fa904e45a11dd8b4db05456f662fa38cf6dd42
Davide D'Ambrosio <davide431@outlook.it>
2023-01-22 18:54:46 +0100

    Correzione di alcuni errori, aggiunte delle risposte a casi particolari, sistemato l'output
    Correzione di alcuni errori, aggiunte delle risposte a casi particolari, sistemato l'output: corretto l'errore del eof, che ciclava una volta in più del dovuto, quindi creava degli errori per la lettura di una riga inesistente; aggiunte delle risposte in caso le macchine di una categoria non fossero disponibili e in caso la macchina selezionata sia stata già affittata per tutta la settimana.

--- main.cpp
+++ main.cpp
@@ -9,10 +9,7 @@ struct macchina{
     char week[7];
 };
 
-string found_car[100];
-int c = 0;
-
-int seleziona_auto(){
+int seleziona_auto(string found_car[100], int c){
     int id = 0;
     for(int i=0; i<c; i++)
         cout<<"ID ["<<i<<"] "<<found_car[i]<<endl;
@@ -23,151 +20,173 @@ int seleziona_auto(){
     }while(id<0 || id>c);
 
     char con;
-    bool c = false;
+    bool q = false;
     do{
     cout<<endl<<"Hai selezionato: "<<found_car[id]<<endl;
     cout<<endl<<"Conferma [C]"<<endl<<"Torna indietro [e]"<<endl<<">>";
     cin>>con;
     if(con != 'C'){
         if(con != 'e')
-            c = true;
+            q = true;
         else
-            c = false;
+            q = false;
     }
     else
-        c = false;
-    }while(c==true);
+        q = false;
+    }while(q==true);
 
     if(con == 'e')
-        seleziona_auto();
+        seleziona_auto(found_car, c);
     else{
         return id;
     }
+    return 0;
 }
 
-void affitta(macchina *car, int id, int x){
+void affitta(macchina *car, int id, int x, string found_car[100]){
     bool pv = false, cg = false;
-    int ris = 0, conta_gd = 0;
+    int ris = 0, conta_gd = 0, cpv = 0, cdd = 0;
     int gd[7];
-    cout<<endl<<"Questi sono i giorni disponibili: "<<endl;
-    do{
-        cout<<endl;
-        for(int i=1; i<8; i++){
-            switch(i){
-                case 1:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Lunedi'"<<endl;
-                    }
-                break;
-                case 2:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Martedi'"<<endl;
-                    }
-                break;
-                case 3:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Mercoledi'"<<endl;
-                    }
-                break;
-                case 4:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Giovedi'"<<endl;
-                    }
-                break;
-                case 5:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Venerdi'"<<endl;
-                    }
-                break;
-                case 6:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Sabato"<<endl;
-                    }
-                break;
-                case 7:
-                    if(car->week[i-1]=='L'){
-                        cout<<"["<<i<<"]"<<"Domenica"<<endl;
-                    }
-                break;
-                default:
-                    cout<<"Errore"<<endl;
-            }
-        }
 
-            if(pv == false){
-                cout<<endl<<"Quale/i giorno/i vuoi affittare?";
-                pv = true;
-            }
-            cout<<endl<<"Inserire 9 per uscire";
-            cout<<endl<<">> ";
-            cin>>ris;
-            if(ris == 9)
-                cg = true;
+    for(int i=0; i<7; i++){
+        if(car->week[i] == 'A')
+            cpv++;
+    }
 
-                if(cg == false){
+    if(cpv != 7){
+        cout<<endl<<"Questi sono i giorni disponibili: "<<endl;
+            do{
+                cdd = 0;
+                for(int i=0; i<7; i++){
+                    if(car->week[i] == 'A')
+                    cdd++;
+                }
+                if(cdd != 7){
+                    cout<<endl;
+                    for(int i=1; i<8; i++){
+                        switch(i){
+                            case 1:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Lunedi'"<<endl;
+                                }
+                            break;
+                            case 2:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Martedi'"<<endl;
+                                }
+                            break;
+                            case 3:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Mercoledi'"<<endl;
+                                }
+                            break;
+                            case 4:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Giovedi'"<<endl;
+                                }
+                            break;
+                            case 5:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Venerdi'"<<endl;
+                                }
+                            break;
+                            case 6:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Sabato"<<endl;
+                                }
+                            break;
+                            case 7:
+                                if(car->week[i-1]=='L'){
+                                    cout<<"["<<i<<"]"<<"Domenica"<<endl;
+                                }
+                            break;
+                            default:
+                                cout<<"Errore"<<endl;
+                        }
+                    }
+                    if(pv == false){
+                        cout<<endl<<"Quale/i giorno/i vuoi affittare?";
+                        pv = true;
+                    }
+                    cout<<endl<<"Inserire 9 per uscire";
+                    cout<<endl<<">> ";
+                    cin>>ris;
+                    if(ris == 9){
+                        cg = true;
+                    }
+                        if(cg == false){
 
-                bool cgd = false;
-                for(int i=0; i<conta_gd; i++){
-                    if(ris == car->week[i])
-                        cgd = true;
-                    else
-                        cgd = false;
+                        bool cgd = false;
+                        for(int i=0; i<conta_gd; i++){
+                            if(ris == car->week[i])
+                                cgd = true;
+                            else
+                                cgd = false;
+                        }
+
+                        if(cgd == false){
+                            switch(ris){
+                                case 1:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 2:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 3:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 4:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 5:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 6:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+
+                                case 7:
+                                    car->week[ris-1] = 'A';
+                                    gd[conta_gd] = ris;
+                                    conta_gd++;
+                                break;
+                            }
+                        }
+                        else
+                            cout<<"Hai inserito un giorno gia' prenotato"<<endl;
+                    }
+                }
+                else{
+                    cout<<endl<<"Questa macchina e' stata affittata per tutta la settimana!"<<endl;
+                    cg = true;
+                }
+            }while(cg == false);
+
+                for(int i=0; i<7; i++){
+                found_car[id][x] = car->week[i];
+                x+=3;
                 }
 
-                if(cgd == false){
-                    switch(ris){
-                        case 1:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 2:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 3:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 4:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 5:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 6:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-
-                        case 7:
-                            car->week[ris-1] = 'A';
-                            gd[conta_gd] = ris;
-                            conta_gd++;
-                        break;
-                    }
-                }
-                else
-                    cout<<"Hai inserito un giorno gia' prenotato"<<endl;
-            }
-        }while(cg == false);
-
-        for(int i=0; i<7; i++){
-        found_car[id][x] = car->week[i];
-        x+=3;
         }
+    else
+        cout<<endl<<"Questa macchina e' stata affittata per tutta la settimana!"<<endl;
 }
 
 void aggiorna_file(string *fo){
@@ -192,74 +211,13 @@ void copia_file(string fo[]){
 
     while(!fin.eof()){
         getline(fin, fo[i]);
+        if(fin.eof())
+            break;
+        else{
         fo[i] = fo[i] + '\n';
         i++;
-    }
-    fin.close();
-}
-
-void cerca_auto(macchina car){
-    ifstream fin("auto.csv", ios::in);
-    if(!fin)
-        cout<<"Errore di apertura del file";
-
-    c = 0;
-    string cat, app, res;
-    char v = ',';
-
-    cout<<endl<<"Inserisci la categoria dell'auto: ";
-    cin>>car.categoria;
-    cout<<endl;
-
-    while(!fin.eof()){
-        fin>>cat;
-        cat.erase(remove(cat.begin(),cat.end(),v),cat.end());   // Formula che serve per eliminare la virgola
-        if(cat == car.categoria){
-            getline(fin, app);
-            res = cat + "," + app;
-            found_car[c] = res;
-            c++;
         }
     }
-
-    int id = seleziona_auto();
-    int cv = 0;
-    int x = 0;
-    do{
-
-       if(found_car[id][x] == ','){
-         cv++;
-         }
-       if(cv == 4){
-            x++;
-        }
-         x++;
-    }while(cv != 4);
-
-    for(int i=0; i<7; i++)
-    {
-        car.week[i] = found_car[id][x];
-        x+=3;
-    }
-    x = x-21;
-
-    string original = found_car[id]+'\n';
-
-    affitta(&car, id, x);
-
-    string fo[200];
-    copia_file(fo);
-
-    bool b = false;
-    int i = 0;
-    while(b != true){
-        if(fo[i] == original){
-            b = true;
-            fo[i] = found_car[id] + '\n';
-        }
-        i++;
-    }
-    aggiorna_file(fo);
     fin.close();
 }
 
@@ -275,6 +233,82 @@ void stampa_file(){
     }
 }
 
+void cerca_auto(macchina car, string found_car[100], int c){
+    ifstream fin("auto.csv", ios::in);
+    if(!fin)
+        cout<<"Errore di apertura del file";
+    c = 0;
+    string cat, app, res;
+    char v = ',';
+
+    stampa_file();
+    cout<<endl<<"Queste e' la lista della auto"<<endl;
+
+    cout<<endl<<"Inserisci la categoria dell'auto: ";
+    cin>>car.categoria;
+    cout<<endl;
+
+    while(!fin.eof()){
+        fin>>cat;
+        if(fin.eof())
+            break;
+        else{
+            cat.erase(remove(cat.begin(),cat.end(),v),cat.end());   // Formula che serve per eliminare la virgola
+            if(cat == car.categoria){
+                getline(fin, app);
+                res = cat + "," + app;
+                found_car[c] = res;
+                c++;
+            }
+            else
+            getline(fin, app);
+        }
+    }
+
+    if(c != 0){
+        int id = seleziona_auto(found_car, c);
+        int cv = 0;
+        int x = 0;
+        do{
+           if(found_car[id][x] == ','){
+             cv++;
+           }
+           if(cv == 4){
+                x++;
+           }
+             x++;
+        }while(cv != 4);
+
+
+        for(int i=0; i<7; i++){
+            car.week[i] = found_car[id][x];
+            x+=3;
+        }
+        x = x-21;
+
+        string original = found_car[id]+'\n';
+
+        affitta(&car, id, x, found_car);
+
+        string fo[200];
+        copia_file(fo);
+
+        bool b = false;
+        int i = 0;
+        while(b != true){
+            if(fo[i] == original){
+                b = true;
+                fo[i] = found_car[id] + '\n';
+            }
+            i++;
+        }
+        aggiorna_file(fo);
+    }
+    else
+        cout<<"Auto non disponibile"<<endl;
+    fin.close();
+}
+
 /*
 utilitaria, FIAT, Panda, rosso, L, L, L, A, A, A, A
 utilitaria, KIA, Pikanto, bronzo, A, A, L, L, L, A, A
@@ -287,6 +321,8 @@ utilitaria, Peugeot, 108, verde, L, A, L, L, L, A, L
 
 void menu(macchina car)
 {
+    string found_car[100];
+    int c = 0;
     int sc;
     do {
     cout << endl;
@@ -299,7 +335,7 @@ void menu(macchina car)
 		switch (sc)
 		{
 			case 1:
-                cerca_auto(car);
+                cerca_auto(car, found_car, c);
 				break;
 			case 2:
 			    stampa_file();
